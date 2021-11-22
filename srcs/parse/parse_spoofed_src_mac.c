318ae3a81b03e34d2312c2586d3e5c82a26231ad
pablo-lamtenzan <pablolamtenzan@gmail.com>
2021-11-22 12:14:24 +0100

    [PARSE] implement --spoof-mac

--- srcs/parse/parse_spoofed_src_mac.c
+++ srcs/parse/parse_spoofed_src_mac.c
@@ -4,11 +4,59 @@
 # include <ft_utils.h>
 # include <ft_libc.h>
 
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+
+# define ISZEROMAC(mac) (                   \
+    (mac)[0] == '0' && (mac)[1] == '0'      \
+    && (mac)[3] == '0' && (mac)[4] == '0'   \
+    && (mac)[6] == '0' && (mac)[7] == '0'   \
+    && (mac)[9] == '0' && (mac)[10] == '0'  \
+    && (mac)[12] == '0' && (mac)[13] == '0' \
+    && (mac)[15] == '0' && (mac)[16] == '0' \
+)
+
+# define ISCOLON(x) ((x) == ':')
+# define ISVALIDMAC(mac) (					\
+	ISHEX((mac)[0]) && ISHEX((mac)[1])		\
+	&& ISCOLON((mac)[2])					\
+	&& ISHEX((mac)[3])	&& ISHEX((mac)[4])	\
+	&& ISCOLON((mac)[5])					\
+	&& ISHEX((mac)[6]) && ISHEX((mac)[7])	\
+	&& ISCOLON((mac)[8])					\
+	&& ISHEX((mac)[9]) && ISHEX((mac)[10])	\
+	&& ISCOLON((mac)[11])					\
+	&& ISHEX((mac)[12])	&& ISHEX((mac)[13])	\
+	&& ISCOLON((mac)[14])					\
+	&& ISHEX((mac)[15]) && ISHEX((mac)[16])	\
+    && ISZEROMAC(mac) == false              \
+    && ((mac)[17] == 0)                     \
+)
+
 err_t   parse_spoofed_src_mac(const char** s, parse_t* const parse)
 {
-    (void)s;
-    (void)parse;
-    return SUCCESS;
+    if (strlen(*s) < 17 || ISVALIDMAC(*s) == false)
+    {
+        PRINT_ERROR(EMSG_INVARG, O_EV_MAC_STR, *s);
+        return EARGUMENT;
+    }
 
-    /// Just validate a MAC and store it in u8[4] or malloc sizeof(u8) * 4
+    char** base = split((char*)*s, ':');
+    if (base == NULL)
+    {
+        PRINT_ERROR(EMSG_SYSCALL, "malloc", errno);
+        return ESYSCALL;
+    }
+
+    parse->args.src_mac[0] = ft_strtol(base[0], 0, 16);
+    parse->args.src_mac[1] = ft_strtol(base[1], 0, 16);
+    parse->args.src_mac[2] = ft_strtol(base[2], 0, 16);
+    parse->args.src_mac[3] = ft_strtol(base[3], 0, 16);
+    parse->args.src_mac[4] = ft_strtol(base[4], 0, 16);
+    parse->args.src_mac[5] = ft_strtol(base[5], 0, 16);
+
+    free_split(base);
+
+    return SUCCESS;
 }
