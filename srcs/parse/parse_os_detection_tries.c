a3bcfb7ed7ded8318c54e9d56c210a59606e1d98
pablo-lamtenzan <pablolamtenzan@gmail.com>
2021-11-22 11:36:38 +0100

    [PARSE] Implement --max-os-tries option

--- srcs/parse/parse_os_detection_tries.c
+++ srcs/parse/parse_os_detection_tries.c
@@ -4,11 +4,27 @@
 # include <ft_utils.h>
 # include <ft_libc.h>
 
+# include <stdlib.h>
+
 err_t   parse_os_detection_tries(const char** s, parse_t* const parse)
 {
-    (void)s;
-    (void)parse;
-    return SUCCESS;
+    for (register u64 i = 0 ; (*s)[i] ; i++)
+    {
+        if (ISNUM((*s)[i]) == false)
+        {
+            PRINT_ERROR(EMSG_INVARG, O_OS_MTR_STR, *s);
+            return EARGUMENT;
+        }
+    }
 
-    // Digit 1 <-> 5
+    const i32 value = atoi(*s);
+
+    if (ISVAL_INRANGE(value, 1, 5) == false)
+    {
+        PRINT_ERROR(EMSG_INV_VALUE, O_OS_MTR_STR, *s, 1, 5);
+        return EARGUMENT;
+    }
+
+    parse->args.os_det_tries = value;
+    return SUCCESS;
 }
