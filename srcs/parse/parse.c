b518fe6156a4e5dbb3983370b5095f9e6ccfee69
pablo-lamtenzan <pablolamtenzan@gmail.com>
2021-11-22 14:29:46 +0100

    [PARSE] Option --scan implemented

--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -12,19 +12,6 @@
 
 err_t parse_all_arguments(const char** av[], parse_t* const parse)
 {
-    static const char* const arg_str[] = {
-        O_HELP_STR,    O_FULLPORT_STR, O_PORT_STR,
-        O_IP_STR,      O_FILE_STR,     O_SPEEDUP_STR, 
-        O_SCAN_STR,    O_VE_UP_STR,    O_VE_LIGHT_STR,
-        O_VE_ALL_STR,  O_OS_UP_STR,    O_OS_LIM_STR,
-        O_OS_GSS_STR,  O_OS_MTR_STR,   O_EV_MTU_STR,
-        O_EV_FRG_STR,  O_EV_DEC_STR,   O_EV_IP_STR,
-        O_EV_IF_STR,   O_EV_SPRT_STR,  O_EV_HDAT_STR,
-        O_EV_SDAT_STR, O_EV_RDAT_STR,  O_EV_IPOP_STR,
-        O_EV_TTL_STR,  O_EV_RHST_STR,  O_EV_MAC_STR,
-        O_EV_BSUM
-    };
-
     static const parse_arg_t arg_f[] = {
         NULL,
         NULL,
--- srcs/parse/parse_scan.c
+++ srcs/parse/parse_scan.c
@@ -4,11 +4,86 @@
 # include <ft_utils.h>
 # include <ft_libc.h>
 
+# include <string.h>
+
 err_t   parse_scan(const char** s, parse_t* const parse)
 {
-    (void)s;
-    (void)parse;
-    return SUCCESS;
+    static const char* const scans[] = {
+        O_S_TCPSYN_STR,
+        O_S_TCPCON_STR,
+        O_S_TCPACK_STR,
+        O_S_TCPWIN_STR,
+        O_S_TCPMAI_STR,
+        O_S_UDP_STR,
+        O_S_TCPNUL_STR,
+        O_S_TCPFIN_STR,
+        O_S_TCPXMA_STR,
+        O_S_TCPCUS_STR,
+        O_S_SCTPIN_STR,
+        O_S_SCTPCE_STR,
+        O_S_IPPROT_STR
+    };
 
-    // Any of possible scan flag options
+    static const char* const alliases[] = {
+        "SYN",
+        "CON",
+        "ACK",
+        "WINDOW",
+        "MAIMON",
+        "UDP",
+        "NULL",
+        "FIN",
+        "XMAS",
+        O_S_TCPCUS_STR,
+        "INIT",
+        "ECHO",
+        "IPPROTO"
+    };
+
+	err_t st = SUCCESS;
+
+	bool found = false;
+	bool found_one = false;
+	for ( ; s && *s ; )
+	{
+		found = false;
+		for ( register u64 i = 0 ; i < ARRAYSIZE(scans) ; i++)
+		{
+			if (strncmp(*s, scans[i], strlen(scans[i]) + 1) == 0
+			|| strncmp(*s, alliases[i], strlen(alliases[i]) + 1) == 0)
+			{
+				found = true;
+				found_one = true;
+				BITADD(parse->opts, 1 << (i + 6));
+				s++;
+				if (s && i == 9)
+					parse_scanflags(s, parse);
+				break ;
+			}
+		}
+
+		if (found == false)
+		{
+			for (register u64 i = 0 ; i < ARRAYSIZE(arg_str) ; i++)
+			{
+				if (strncmp(*s, arg_str[i], strlen(arg_str[i]) + 1) == 0)
+				{
+					found = true;
+					break ;
+				}
+			}
+			break ;
+		}
+	}
+
+	if (found == false || found_one == false)
+	{
+		if (found_one == false && found == true)
+			PRINT_ERROR(EMSG_EXPECTED_ARG, O_SCAN_STR);
+		else
+			PRINT_ERROR(EMSG_INVARG, O_SCAN_STR, *s);
+		st = EARGUMENT;
+	}
+	s--;
+	return st;
 }
