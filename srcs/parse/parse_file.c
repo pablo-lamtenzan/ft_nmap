3019640a6721847956e74e2a472f6d35ac29b9c1
pablo-lamtenzan <pablolamtenzan@gmail.com>
2021-11-22 11:14:46 +0100

    [PARSE] --file option implemented

--- srcs/parse/parse_file.c
+++ srcs/parse/parse_file.c
@@ -4,15 +4,68 @@
 # include <ft_utils.h>
 # include <ft_libc.h>
 
-err_t   parse_file(const char** s, parse_t* const parse)
+# include <errno.h>
+# include <stdlib.h>
+# include <string.h>
+
+# define CPCAP 1024
+
+err_t	parse_file(const char** s, parse_t* const parse)
 {
-    (void)s;
-    (void)parse;
-    return SUCCESS;
+	FILE* fp = fopen(*s, "r");
+	if (fp == NULL)
+	{
+		if (errno == EINVAL)
+		{
+			PRINT_ERROR(EMSG_UNKOWN_FILENAME, *s);
+			return EARGUMENT;
+		}
+		else
+		{
+			PRINT_ERROR(EMSG_SYSCALL, "fopen", errno);
+			return ESYSCALL;
+		}
+		
+	}
 
-    //  maybe redefine the option name for error handing (i kind of template in cpp behaviour)
+	i8* buff = malloc(sizeof(i8) * (CPCAP + 1));
+	if (buff == NULL)
+	{
+		fclose(fp);
+		PRINT_ERROR(EMSG_SYSCALL, "malloc", errno);
+		return ESYSCALL;
+	}
 
-    // 1) Open file, read, cp removing '\n'
-    // 2) Call parse_ips
-    // 3) Store the file pointer
+	register size_t readbytes = 0;
+	u64 exp = 1;
+	for (register u64 i = 0 ; fread(&buff[i], sizeof(*buff), 1, fp) == 1 ; )
+	{
+		if (buff[i] != '\n')
+		{
+			readbytes++; 
+			if (++i > CPCAP * exp)
+			{
+				i8* auxbuff = malloc(sizeof(*buff) * (CPCAP * ++exp));
+				if (auxbuff == NULL)
+				{
+					fclose(fp);
+					free(buff);
+					PRINT_ERROR(EMSG_SYSCALL, "malloc", errno);
+					return ESYSCALL;
+				}
+				memcpy(auxbuff, buff, CPCAP * (exp - 1));
+				free(buff);
+				buff = auxbuff;
+			}
+		}
+	}
+	buff[readbytes] = 0;
+
+	fclose(fp);
+
+	err_t st = parse_ips((const char**)&buff, parse);
+
+	parse->args.file = buff;
+
+	return st;
 }
