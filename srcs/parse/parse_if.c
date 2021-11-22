bd973a065328054c1fdcfe441b2dddc84be47743
pablo-lamtenzan <pablolamtenzan@gmail.com>
2021-11-22 12:52:35 +0100

    [PARSE] implement -e option

--- srcs/parse/parse_if.c
+++ srcs/parse/parse_if.c
@@ -4,12 +4,38 @@
 # include <ft_utils.h>
 # include <ft_libc.h>
 
+# include <ifaddrs.h>
+# include <net/if.h>
+# include <errno.h>
+# include <string.h>
+
 err_t   parse_if(const char** s, parse_t* const parse)
 {
-    (void)s;
-    (void)parse;
-    return SUCCESS;
+    struct ifaddrs* ifap = NULL;
+    bool found = false;
 
-    /// Disable auto-interface selection
-    /// Take a string that must be a valid interface
+    if (getifaddrs(&ifap) < 0)
+    {
+        PRINT_ERROR(EMSG_SYSCALL, "getifaddrs", errno);
+        return EARGUMENT;
+    }
+
+    for (struct ifaddrs* i = ifap ; i->ifa_next ; i = i->ifa_next)
+    {
+        if (i->ifa_name == NULL || i->ifa_addr->sa_family != AF_INET)
+            continue ;
+        if (strncmp(i->ifa_name, *s, strlen(i->ifa_name) + 1) == 0)
+        {
+            if (i->ifa_flags & IFF_UP)
+                found = true;
+        }
+    }
+
+    if (found == false)
+        PRINT_ERROR(EMSG_INVARG, O_EV_IF_STR, *s);
+    else
+        parse->args.interface = *s;
+
+    freeifaddrs(ifap);
+    return found == true ? SUCCESS : EARGUMENT;
 }
