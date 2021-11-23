434520a466fd84e9258f4eb61906121e612c4172
pablo-lamtenzan <pablolamtenzan@gmail.com>
2021-11-23 17:33:25 +0100

    update sources

--- srcs.mk
+++ srcs.mk
@@ -4,9 +4,11 @@ SRCDIR	=	srcs
 HDRS	=\
 $(addprefix includes/,\
 	debug.h\
+	ft_engine.h\
 	ft_error.h\
 	ft_libc.h\
 	ft_nmap.h\
+	ft_packet_crafting.h\
 	ft_parse.h\
 	ft_types.h\
 	ft_utils.h\
@@ -21,12 +23,33 @@ $(addprefix srcs/,\
 		dbg_print_ports.c\
 	)\
 	$(addprefix engine/,\
+		free_routine_data.c\
+		gen_routine_data.c\
+		process_all_hosts.c\
+		process_host.c\
+		process_host_port.c\
 	)\
 	$(addprefix ft_libc/,\
 		ft_strtol.c\
 		split.c\
 	)\
 	main.c\
+	$(addprefix packet_crafting/,\
+		free_pk_craft.c\
+		pk_craftipproto.c\
+		pk_craftscpt_cookie_echo.c\
+		pk_craftscpt_init.c\
+		pk_craft_tcp_ack.c\
+		pk_crafttcp_custom.c\
+		pk_crafttcp_fin.c\
+		pk_craft_tcp_maimin.c\
+		pk_craft_tcp_syn.c\
+		pk_craft_tcp_win.c\
+		pk_crafttcp_xmas.c\
+		pk_craftter.c\
+		pk_craft_tmp_nul.c\
+		pk_craft_udp.c\
+	)\
 	$(addprefix parse/,\
 		free_parse.c\
 		parse.c\
